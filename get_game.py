35d50e600d321f54e7628ec910989fb4f1547823
Robert Alland <robert_alland@intuit.com>
2019-11-06 13:55:15 -0800

    Add get_game lambda

---
+++ get_game.py
@@ -0,0 +1,15 @@
+import os
+
+from models import Game
+from db_util import db_session
+from lambda_decorators import ssm_parameter_store, json_http_resp, load_json_body, cors_headers
+
+@cors_headers(origin="*.intuit.com")
+@json_http_resp
+@ssm_parameter_store('/prod/camelot/db-password')
+def lambda_handler(event, context):
+    game = {}
+    gameId = event['pathParameters']['gameId']
+    with db_session(os.environ['HOST'], context.parameters['/prod/camelot/db-password']) as session:
+        game = as_dict(session.query(Game).filter(Game.id == gameId).first())
+    return game
