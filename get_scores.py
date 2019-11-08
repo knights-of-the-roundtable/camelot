3020c5558b687990d22a877b21ac852aad284e9f
Vivek Jain <Vivek_Jain@intuit.com>
2019-11-08 11:17:16 -0800

    update scores

--- get_scores.py
+++ get_scores.py
@@ -11,6 +11,17 @@ def add_point(players, player):
 def subtract_point(players, player):
     players[player]['score'] -= 1
 
+def add_win(players, player):
+    add_point(players, player)
+    players[player]['win'] += 1
+
+def add_loss(players, player):
+    players[player]['loss'] += 1
+
+def update_totals(players, player):
+    players[player]['games'] = players[player]['win'] + players[player]['loss']
+    players[player]['percentage'] = 0 if players[player]['games'] == 0 else players[player]['win'] / players[player]['games']
+
 # Order of decorators matters!
 @cors_headers(origin="https://uxfabric-e2e.app.intuit.com")
 @json_http_resp
@@ -19,7 +30,7 @@ def lambda_handler(event, context):
     body = {}
     with db_session(os.environ['HOST'], context.parameters['/prod/camelot/db-password']) as session:
         # Setup players map, and give each player a starting score of 0 in case they haven't played any games
-        players = {player.id:{'id': player.id, 'name': player.full_name(), 'score':0} for player in session.query(Player).all()}
+        players = {player.id:{'id': player.id, 'name': player.full_name(), 'score':0, 'win':0, 'loss':0} for player in session.query(Player).all()}
         # Handle points from each game
         for game in session.query(Game).all():
             # Handle MVP and LVP points for this game
@@ -28,7 +39,11 @@ def lambda_handler(event, context):
             # Handle all the winners for this game
             for gamePlayer in game.game_players:
                 if gamePlayer.role.is_good() == game.outcome.is_good():
-                    add_point(players, gamePlayer.player_id)
+                    add_win(players, gamePlayer.player_id)
+                else:
+                    add_loss(players, gamePlayer.player_id)
+                update_totals(players, gamePlayer.player_id)
+
         body = list(players.values())
         body.sort(key=itemgetter('score'), reverse=True)
     return body
