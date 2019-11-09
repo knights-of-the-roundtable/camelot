bc6451d2560e8eb2f2e84c6a12693d4b5132887d
Joel Jauregui <joel.jauregui@gmail.com>
2019-11-08 22:48:23 -0800

    Missed migrating add_player

--- add_player.py
+++ add_player.py
@@ -1,17 +1,14 @@
-import os
-
 from models import Player
 from db_util import db_session
-from lambda_decorators import ssm_parameter_store, json_http_resp, load_json_body, cors_headers
+from lambda_decorators import json_http_resp, load_json_body, cors_headers
 
 @cors_headers(origin="https://uxfabric-e2e.app.intuit.com")
 @json_http_resp
-@ssm_parameter_store('/prod/camelot/db-password')
 @load_json_body
 def lambda_handler(event, context):
     body = {}
     request = event['body']
-    with db_session(os.environ['HOST'], context.parameters['/prod/camelot/db-password']) as session:
+    with db_session() as session:
         player = Player(first_name=request['first_name'], last_name=request['last_name'])
         session.add(player)
         session.commit()
